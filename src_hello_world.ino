f1566b6f59ffdc23d0ad321a27b9dbb3b323ed96
hcz0088 <haz296@pitt.edu>
2025-10-15 10:59:50 -0400

    Create src_hello_world.ino

---
+++ src_hello_world.ino
@@ -0,0 +1,16 @@
+/* Contributor: Hanchen Zhu
+ * Hello World – 1_led
+ */
+const uint8_t LED_PIN = 26;
+const uint8_t BTN_PIN = 2; 
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT_PULLUP); 
+  digitalWrite(LED_PIN, LOW);
+}
+
+void loop() {
+  bool pressed = (digitalRead(BTN_PIN) == LOW);
+  digitalWrite(LED_PIN, pressed ? HIGH : LOW);
+}
